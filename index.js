81983116388c4625248e28b2c2a35b275475cdff
Kesha <kesha.marges@gmail.com>
2022-12-13 14:04:40 -0500

    commit tree 9f281e66f626d32587bfb0e563be2955d4d93c59

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+// const win = item => item.result === "W"
+
+function superbowlWin(item) {
+    const winningYear = item.find(item => item.result === "W")
+        if (winningYear) {
+            return winningYear.year;
+       } else {
+        return undefined};
+}
